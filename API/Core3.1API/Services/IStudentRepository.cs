fa3a84f76d1073ae0bbd9cc69c8184463731e620
sonu kumar <63550598+sonuku58@users.noreply.github.com>
2021-12-02 12:27:12 +0530

    Add files via upload

---
+++ API/Core3.1API/Services/IStudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using core3._1api.DTOs;
+using core3._1api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core3._1api.Services
+{
+     public interface IStudentRepository
+    {
+  
+        IEnumerable<Student> GetAllStudent();
+
+        Student StudentById(int Id);
+
+        void AddStudent(Student student);
+
+        void UpdateStudent(int Id,Student student);
+        void UpdateStudent(Student student);
+
+        void RemoveStudent( Student student);
+    }
+}
---
+++ API/Core3.1API/Services/StudentRepository.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using core3._1api.DTOs;
+using core3._1api.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core3._1api.Services
+{
+    public class StudentRepository : IStudentRepository
+    {
+
+        private readonly ApplicationDbContext context;
+       
+        public StudentRepository( ApplicationDbContext _context )
+        {
+            this.context = _context;
+            
+        }
+
+     
+
+        public  IEnumerable<Student> GetAllStudent()
+        {
+            var studentList = context.Students.AsNoTracking().ToList();
+           
+            return studentList;
+        }
+
+        public Student StudentById(int Id)
+        {
+            return  context.Students.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public void AddStudent(Student student)
+        {
+            context.Add(student);
+            context.SaveChanges();
+        }
+
+        public void UpdateStudent(int Id,Student student)
+        {
+           
+            var students = context.Students.FirstOrDefault(s => s.Id == Id);
+            students.FirstName = student.FirstName;
+            students.LastName = student.LastName;
+            students.Gender = student.Gender;
+            students.Salary = student.Salary;
+            context.SaveChanges();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            var students = context.Students.FirstOrDefault(s => s.Id == student.Id);
+            students.FirstName = student.FirstName;
+            students.LastName = student.LastName;
+            students.Gender = student.Gender;
+            students.Salary = student.Salary;
+            context.SaveChanges();
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            context.Remove(student);
+            context.SaveChanges();
+        }
+
+        
+    }
+}
